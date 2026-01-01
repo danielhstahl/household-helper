9b31e36f255406581c5d49ec32bbac948e345a60
Daniel Stahl <danstahl1138@gmail.com>
2026-01-01 16:12:18 -0600

    fix comma

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -94,6 +94,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
   </StrictMode>,
 );
